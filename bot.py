429c81034230f475578f786e1195402e4a089df5
chamith araliya <166959556+Chamithare@users.noreply.github.com>
2026-02-19 00:18:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNELS, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNELS, CHANNEL_ID, PORT, JOIN_REQUEST_ENABLE
 
 
 ascii_art = """
@@ -50,10 +50,17 @@ class Bot(Client):
                     if force_sub_chat.username:
                         link = f"https://t.me/{force_sub_chat.username}"
                     else:
-                        link = force_sub_chat.invite_link
-                        if not link:
-                            await self.export_chat_invite_link(force_sub_chat_id)
-                            link = (await self.get_chat(force_sub_chat_id)).invite_link
+                        if JOIN_REQUEST_ENABLE:
+                            invite = await self.create_chat_invite_link(
+                                chat_id=force_sub_chat_id,
+                                creates_join_request=True
+                            )
+                            link = invite.invite_link
+                        else:
+                            link = force_sub_chat.invite_link
+                            if not link:
+                                await self.export_chat_invite_link(force_sub_chat_id)
+                                link = (await self.get_chat(force_sub_chat_id)).invite_link
 
                     if link:
                         self.force_sub_join_links[force_sub_chat_id] = link
